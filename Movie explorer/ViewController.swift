43a4a3c666213ae2295cfe5263c17d38f25133b4
humayun-ch <kabirchanchal07@gmail.com>
2025-03-04 13:46:56 +0600

    Initial Commit

---
+++ Movie explorer/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Movie explorer
+//
+//  Created by Humayun Kabir on 4/3/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Movie explorerTests/Movie_explorerTests.swift
@@ -0,0 +1,17 @@
+//
+//  Movie_explorerTests.swift
+//  Movie explorerTests
+//
+//  Created by Humayun Kabir on 4/3/25.
+//
+
+import Testing
+@testable import Movie_explorer
+
+struct Movie_explorerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
